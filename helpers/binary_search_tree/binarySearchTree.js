9b169da67f68f7a094840c3f1d0e9e7a8362fc98
Gage <gagemigan@gmail.com>
2020-08-26 09:28:50 -0400

    start up (added tree & queue)

---
+++ helpers/binary_search_tree/binarySearchTree.js
@@ -0,0 +1,36 @@
+const Queue = require('../queue/queue');
+
+module.exports = class BinarySearchTree {
+  constructor(key =null, value = null, parent = null){
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+  dfs(values=[]){
+    if(this.left){
+      values = this.left.dft(values);
+    }
+    values.push(this.value);
+
+    if(this.right) {
+      values = this.right.dfs(values);
+    }
+    return values;
+  }
+  bfs(tree , values = []){
+    const queue = new Queue();
+    const node = tree.root;
+    queue.enqueue(node);
+    while (queue.length) {
+      const node = queue.dequeue();
+      values.push(node.value);
+      if (node.left) queue.enqueue(node.left);
+
+      if(node.right) queue.enqueue(node.right);
+    }
+    return values;
+  }
+
+};
